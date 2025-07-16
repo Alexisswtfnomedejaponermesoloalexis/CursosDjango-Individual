e2f88224b672d441716604a165323983878d0861
bluegglweed <bluegglweed@gmail.com~>
2025-07-15 19:25:33 -0600

    comitss

--- CursosDjango/cursos/admin.py
+++ CursosDjango/cursos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Cursos
-from .models import Becas
-from .models import Actividad
+from .models import Becas, Comentario
+from .models import Actividad, ComentarioContacto
 
 class AdministrarModelo(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
@@ -32,3 +32,20 @@ class AdministrarActividad(admin.ModelAdmin):
     readonly_fields = ('created', 'id_act')
     list_filter = ('id_act', 'created')
 admin.site.register(Actividad, AdministrarActividad)
+
+class AdministarComentarios(admin.ModelAdmin):
+    list_display = ('id', 'coment')
+    search_fields = ('id', 'created')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+
+admin.site.register(Comentario, AdministarComentarios)
+
+
+class AdministrarComentariosContacto(admin.ModelAdmin):
+    list_display = ('id', 'mensaje')
+    search_fields = ('id','created')
+    date_hierarchy = 'created'
+    readonly_fields = ('created', 'id')
+
+admin.site.register(ComentarioContacto, AdministrarComentariosContacto)
--- CursosDjango/cursos/models.py
+++ CursosDjango/cursos/models.py
@@ -73,3 +73,18 @@ class ComentarioContacto(models.Model):
 
     def __str__(self):
         return self.mensaje
+    
+    
+class Comentario(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Clave")
+    curso = models.ForeignKey(Cursos, on_delete=models.CASCADE, verbose_name="Curso")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Registrado")
+    coment = RichTextField(verbose_name="Comentario")
+
+    class Meta:
+        verbose_name = 'Comentario'
+        verbose_name_plural = 'Comentarios'
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.coment
