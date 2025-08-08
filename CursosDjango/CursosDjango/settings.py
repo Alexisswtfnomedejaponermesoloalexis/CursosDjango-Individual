9c111be80f3704a38d4b255b1e6057e6dd7c3332
bluegglweed <bluegglweed@gmail.com~>
2025-08-07 20:31:34 -0600

    modified settings

--- CursosDjango/CursosDjango/settings.py
+++ CursosDjango/CursosDjango/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'CursosDjango.urls'
-import os 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
